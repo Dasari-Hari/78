6a1442ccef7131417659f29a19dce9714ddcd2fe
Dasari Hari Komal Raj <82033233+Dasari-Hari@users.noreply.github.com>
2021-07-24 10:05:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+var img=[""
+
+]
+ var   name=["Family Book","Dasari Hari",""]
+ var j=0;
+  function update() {
+
+    i++;
+    var family name =5
+if (i=> family name)
+{
+    i = 0;
+}
+var  idatemg =img[i];
+var datename =name[i];
+document.getElementById("rr").src = idatemg;
+document.getElementById("FAMLYBOOK").innerHTML = name;
+
+  }
